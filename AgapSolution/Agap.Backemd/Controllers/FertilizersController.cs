ffbf17c979cd8d2f952ea007da5f4b8137f1c630
ajvillav <ajvillav@unal.edu.co>
2023-09-17 14:58:24 -0500

    Add filter and pagination to frontend

--- AgapSolution/Agap.Backemd/Controllers/FertilizersController.cs
+++ AgapSolution/Agap.Backemd/Controllers/FertilizersController.cs
@@ -1,7 +1,10 @@
 ﻿using Agap.Backemd.Data;
 using Agap.Backemd.Interfaces;
+using Agap.Shared.DTOs;
 using Agap.Shared.Entities;
+using Agap.Shared.Helpers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Agap.Backemd.Controllers
 {
@@ -9,9 +12,41 @@ namespace Agap.Backemd.Controllers
     [Route("api/[controller]")]
     public class FertilizersController : GenericController<Fertilizer>
     {
-        public FertilizersController(IGenericUnitOfWork<Fertilizer> unitOfWork, DataContext context) : base(unitOfWork, context)
+        private readonly DataContext _context;
 
+        public FertilizersController(IGenericUnitOfWork<Fertilizer> unitOfWork, DataContext context) : base(unitOfWork, context)
         {
+            _context = context;
+        }
+
+        [HttpGet]
+        public override async Task<IActionResult> GetAsync([FromQuery] PaginationDTO pagination)
+        {
+            var queryable = _context.Fertilizers.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(pagination.Filter))
+            {
+                queryable = queryable.Where(x => x.Name.ToLower().Contains(pagination.Filter.ToLower()));
+            }
+
+            return Ok(await queryable
+                .OrderBy(x => x.Name)
+                .Paginate(pagination)
+                .ToListAsync());
+        }
+
+
+        [HttpGet("totalPages")]
+        public override async Task<ActionResult> GetPagesAsync([FromQuery] PaginationDTO pagination)
+        {
+            var queryable = _context.Fertilizers.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(pagination.Filter))
+            {
+                queryable = queryable.Where(x => x.Name.ToLower().Contains(pagination.Filter.ToLower()));
+            }
+
+            double count = await queryable.CountAsync();
+            double totalPages = Math.Ceiling(count / pagination.RecordsNumber);
+            return Ok(totalPages);
         }
     }
 }
