fcd4fb666b2e779cd1765959f2d6f0deb411e696
Sebastian Silva <sesilvavi@gmail.com>
2023-11-11 12:05:58 -0600

    Pesticide y Fertilizer sin CropType en Front

---
+++ AgapSolution/Agap.Backemd/Migrations/20231107025112_CRUDCropType.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Agap.Backemd.Migrations
+{
+    /// <inheritdoc />
+    public partial class CRUDCropType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
