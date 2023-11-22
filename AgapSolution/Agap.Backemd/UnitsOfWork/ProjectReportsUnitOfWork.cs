e1f69919a575b743938ed8436fa7c0e79579c849
Johan Orozco <johanorozcoroa@gmail.com>
2023-11-22 09:11:50 -0600

    Interface IProjectReportsUnitOfWork and class ProjectReportsUnitOfWork were created

---
+++ AgapSolution/Agap.Backemd/UnitsOfWork/ProjectReportsUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using Agap.Backemd.Repositories;
+using Agap.Shared.DTOs;
+using Agap.Shared.Entities;
+using Agap.Shared.Responses;
+
+namespace Agap.Backemd.UnitsOfWork
+{
+    public class ProjectReportsUnitOfWork : GenericUnitOfWork<ProjectReport>, IProjectReportsUnitOfWork
+    {
+        private readonly IProjectReportsRepository _projectReportsRepository;
+
+        public ProjectReportsUnitOfWork(IGenericRepository<ProjectReport> repository, IProjectReportsRepository projectReportsRepository) : base(repository)
+        {
+            _projectReportsRepository = projectReportsRepository;
+        }
+
+        public override async Task<Response<IEnumerable<ProjectReport>>> GetAsync(PaginationDTO pagination) => await _projectReportsRepository.GetAsync(pagination);
+
+        public override async Task<Response<int>> GetTotalPagesAsync(PaginationDTO pagination) => await _projectReportsRepository.GetTotalPagesAsync(pagination);
+    }
+}
