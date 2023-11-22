065145d5f71a077e3a594278d2e7a624fafb5929
Johan Orozco <johanorozcoroa@gmail.com>
2023-11-22 13:51:37 -0600

    Both ICropReportsUnitOfWork and CropReportsUnitOfWork were created

---
+++ AgapSolution/Agap.Backemd/UnitsOfWork/CropReportsUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using Agap.Backemd.Repositories;
+using Agap.Shared.DTOs;
+using Agap.Shared.Entities;
+using Agap.Shared.Responses;
+
+namespace Agap.Backemd.UnitsOfWork
+{
+    public class CropReportsUnitOfWork : GenericUnitOfWork<CropReport>, ICropReportsUnitOfWork
+    {
+        private readonly ICropReportsRepository _cropReportsRepository;
+
+        public CropReportsUnitOfWork(IGenericRepository<CropReport> repository, ICropReportsRepository cropReportsRepository) : base(repository)
+        {
+            _cropReportsRepository = cropReportsRepository;
+        }
+
+        public override async Task<Response<IEnumerable<CropReport>>> GetAsync(PaginationDTO pagination) => await _cropReportsRepository.GetAsync(pagination);
+
+        public override async Task<Response<int>> GetTotalPagesAsync(PaginationDTO pagination) => await _cropReportsRepository.GetTotalPagesAsync(pagination);
+    }
+}
