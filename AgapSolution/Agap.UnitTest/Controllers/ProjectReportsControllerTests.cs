1e72076bd1eaee298cb6d49e942fa3f3b4fa12d7
Johan Orozco <johanorozcoroa@gmail.com>
2023-11-23 18:28:38 -0600

    Unit Test for ProjectsController and ProjectReportsController

---
+++ AgapSolution/Agap.UnitTest/Controllers/ProjectReportsControllerTests.cs
@@ -0,0 +1,100 @@
+﻿using Agap.Backemd.Controllers;
+using Agap.Backemd.UnitsOfWork;
+using Agap.Shared.DTOs;
+using Agap.Shared.Entities;
+using Agap.Shared.Responses;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace Agap.UnitTest.Controllers
+{
+    [TestClass]
+    public class ProjectReportsControllerTests
+    {
+        private Mock<IGenericUnitOfWork<ProjectReport>> _mockGenericUnitOfWork;
+        private Mock<IProjectReportsUnitOfWork> _mockProjectReportsUnitOfWork;
+        private ProjectReportsController _controller;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _mockGenericUnitOfWork = new Mock<IGenericUnitOfWork<ProjectReport>>();
+            _mockProjectReportsUnitOfWork = new Mock<IProjectReportsUnitOfWork>();
+            _controller = new ProjectReportsController(_mockGenericUnitOfWork.Object, _mockProjectReportsUnitOfWork.Object);
+        }
+
+        [TestMethod]
+        public async Task GetAsync_Pagination_ShouldReturnOk()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var projectReports = new List<ProjectReport>
+            {
+                new ProjectReport { Id = 1, ProjectId = 1, TotalSale = 100, TotalBudget = 200 },
+                new ProjectReport { Id = 2, ProjectId = 2, TotalSale = 150, TotalBudget = 250 }
+            };
+            var response = new Response<IEnumerable<ProjectReport>> { WasSuccess = true, Result = projectReports };
+            _mockProjectReportsUnitOfWork.Setup(x => x.GetAsync(pagination)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetAsync(pagination);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(projectReports, okResult?.Value);
+            _mockProjectReportsUnitOfWork.Verify(x => x.GetAsync(pagination), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetAsync_Pagination_ShouldReturnBadRequest()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var response = new Response<IEnumerable<ProjectReport>> { WasSuccess = false };
+            _mockProjectReportsUnitOfWork.Setup(x => x.GetAsync(pagination)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetAsync(pagination);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+            _mockProjectReportsUnitOfWork.Verify(x => x.GetAsync(pagination), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetPagesAsync_ShouldReturnOk()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var totalPages = 5;
+            var response = new Response<int> { WasSuccess = true, Result = totalPages };
+            _mockProjectReportsUnitOfWork.Setup(x => x.GetTotalPagesAsync(pagination)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetPagesAsync(pagination);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(totalPages, okResult?.Value);
+            _mockProjectReportsUnitOfWork.Verify(x => x.GetTotalPagesAsync(pagination), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetPagesAsync_ShouldReturnBadRequest()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var response = new Response<int> { WasSuccess = false };
+            _mockProjectReportsUnitOfWork.Setup(x => x.GetTotalPagesAsync(pagination)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetPagesAsync(pagination);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+            _mockProjectReportsUnitOfWork.Verify(x => x.GetTotalPagesAsync(pagination), Times.Once());
+        }
+    }
+}
---
+++ AgapSolution/Agap.UnitTest/Controllers/ProjectsControllerTests.cs
@@ -0,0 +1,137 @@
+﻿using Agap.Backemd.Controllers;
+using Agap.Backemd.UnitsOfWork;
+using Agap.Shared.DTOs;
+using Agap.Shared.Entities;
+using Agap.Shared.Responses;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace Agap.UnitTest.Controllers
+{
+    [TestClass]
+    public class ProjectsControllerTests
+    {
+        private Mock<IGenericUnitOfWork<Project>> _mockGenericUnitOfWork;
+        private Mock<IProjectsUnitOfWork> _mockProjectsUnitOfWork;
+        private ProjectsController _controller;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _mockGenericUnitOfWork = new Mock<IGenericUnitOfWork<Project>>();
+            _mockProjectsUnitOfWork = new Mock<IProjectsUnitOfWork>();
+            _controller = new ProjectsController(_mockGenericUnitOfWork.Object, _mockProjectsUnitOfWork.Object);
+        }
+
+        [TestMethod]
+        public async Task GetAsync_Pagination_ShouldReturnOk()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var projects = new List<Project>
+            {
+                new Project { Id = 1, Name = "Project1" },
+                new Project { Id = 2, Name = "Project2" }
+            };
+            var response = new Response<IEnumerable<Project>> { WasSuccess = true, Result = projects };
+            _mockProjectsUnitOfWork.Setup(x => x.GetAsync(pagination)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetAsync(pagination);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(projects, okResult?.Value);
+            _mockProjectsUnitOfWork.Verify(x => x.GetAsync(pagination), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetAsync_Pagination_ShouldReturnBadRequest()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var response = new Response<IEnumerable<Project>> { WasSuccess = false };
+            _mockProjectsUnitOfWork.Setup(x => x.GetAsync(pagination)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetAsync(pagination);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+            _mockProjectsUnitOfWork.Verify(x => x.GetAsync(pagination), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetAsync_ById_ShouldReturnOk()
+        {
+            // Arrange
+            var projectId = 1;
+            var project = new Project { Id = projectId, Name = "Project1" };
+            var response = new Response<Project> { WasSuccess = true, Result = project };
+            _mockProjectsUnitOfWork.Setup(x => x.GetAsync(projectId)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetAsync(projectId);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(project, okResult?.Value);
+            _mockProjectsUnitOfWork.Verify(x => x.GetAsync(projectId), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetAsync_ById_ShouldReturnNotFound()
+        {
+            // Arrange
+            var projectId = 1;
+            var response = new Response<Project> { WasSuccess = false, Message = "Not Found" };
+            _mockProjectsUnitOfWork.Setup(x => x.GetAsync(projectId)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetAsync(projectId);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(NotFoundObjectResult));
+            var notFoundResult = result as NotFoundObjectResult;
+            Assert.AreEqual("Not Found", notFoundResult?.Value);
+            _mockProjectsUnitOfWork.Verify(x => x.GetAsync(projectId), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetPagesAsync_ShouldReturnOk()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var totalPages = 5;
+            var response = new Response<int> { WasSuccess = true, Result = totalPages };
+            _mockProjectsUnitOfWork.Setup(x => x.GetTotalPagesAsync(pagination)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetPagesAsync(pagination);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(totalPages, okResult?.Value);
+            _mockProjectsUnitOfWork.Verify(x => x.GetTotalPagesAsync(pagination), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetPagesAsync_ShouldReturnBadRequest()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var response = new Response<int> { WasSuccess = false };
+            _mockProjectsUnitOfWork.Setup(x => x.GetTotalPagesAsync(pagination)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetPagesAsync(pagination);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+            _mockProjectsUnitOfWork.Verify(x => x.GetTotalPagesAsync(pagination), Times.Once());
+        }
+    }
+}
