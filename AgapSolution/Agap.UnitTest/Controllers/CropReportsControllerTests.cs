8414e6f26c8d0311424ae9d2d42ad4225961e06c
Johan Orozco <johanorozcoroa@gmail.com>
2023-11-23 20:09:56 -0600

    Tests for CropReportsController, CropTypesController, CropsController, ExpensesController, NotificacionsController and PesticidesController

---
+++ AgapSolution/Agap.UnitTest/Controllers/CropReportsControllerTests.cs
@@ -0,0 +1,96 @@
+﻿using Agap.Backemd.Controllers;
+using Agap.Backemd.UnitsOfWork;
+using Agap.Shared.DTOs;
+using Agap.Shared.Entities;
+using Agap.Shared.Responses;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace Agap.UnitTest.Controllers
+{
+    [TestClass]
+    public class CropReportsControllerTests
+    {
+        private Mock<IGenericUnitOfWork<CropReport>> _mockGenericUnitOfWork;
+        private Mock<ICropReportsUnitOfWork> _mockCropReportsUnitOfWork;
+        private CropReportsController _controller;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _mockGenericUnitOfWork = new Mock<IGenericUnitOfWork<CropReport>>();
+            _mockCropReportsUnitOfWork = new Mock<ICropReportsUnitOfWork>();
+            _controller = new CropReportsController(_mockGenericUnitOfWork.Object, _mockCropReportsUnitOfWork.Object);
+        }
+
+        [TestMethod]
+        public async Task GetAsync_Pagination_ShouldReturnOk()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var cropReports = new List<CropReport> { /* Populate with test data */ };
+            var response = new Response<IEnumerable<CropReport>> { WasSuccess = true, Result = cropReports };
+            _mockCropReportsUnitOfWork.Setup(x => x.GetAsync(pagination)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetAsync(pagination);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(cropReports, okResult?.Value);
+            _mockCropReportsUnitOfWork.Verify(x => x.GetAsync(pagination), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetAsync_Pagination_ShouldReturnBadRequest()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var response = new Response<IEnumerable<CropReport>> { WasSuccess = false };
+            _mockCropReportsUnitOfWork.Setup(x => x.GetAsync(pagination)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetAsync(pagination);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+            _mockCropReportsUnitOfWork.Verify(x => x.GetAsync(pagination), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetPagesAsync_ShouldReturnOk()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var totalPages = 5;
+            var response = new Response<int> { WasSuccess = true, Result = totalPages };
+            _mockCropReportsUnitOfWork.Setup(x => x.GetTotalPagesAsync(pagination)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetPagesAsync(pagination);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(totalPages, okResult?.Value);
+            _mockCropReportsUnitOfWork.Verify(x => x.GetTotalPagesAsync(pagination), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetPagesAsync_ShouldReturnBadRequest()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var response = new Response<int> { WasSuccess = false };
+            _mockCropReportsUnitOfWork.Setup(x => x.GetTotalPagesAsync(pagination)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetPagesAsync(pagination);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+            _mockCropReportsUnitOfWork.Verify(x => x.GetTotalPagesAsync(pagination), Times.Once());
+        }
+    }
+}
---
+++ AgapSolution/Agap.UnitTest/Controllers/CropTypesControllerTests.cs
@@ -0,0 +1,100 @@
+﻿using Agap.Backemd.Controllers;
+using Agap.Backemd.UnitsOfWork;
+using Agap.Shared.DTOs;
+using Agap.Shared.Entities.Agap.Shared.Entities;
+using Agap.Shared.Responses;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace Agap.UnitTest.Controllers
+{
+    [TestClass]
+    public class CropTypesControllerTests
+    {
+        private Mock<IGenericUnitOfWork<CropType>> _mockGenericUnitOfWork;
+        private Mock<ICropTypesUnitOfWork> _mockCropTypesUnitOfWork;
+        private CropTypesController _controller;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _mockGenericUnitOfWork = new Mock<IGenericUnitOfWork<CropType>>();
+            _mockCropTypesUnitOfWork = new Mock<ICropTypesUnitOfWork>();
+            _controller = new CropTypesController(_mockGenericUnitOfWork.Object, _mockCropTypesUnitOfWork.Object);
+        }
+
+        [TestMethod]
+        public async Task GetAsync_Pagination_ShouldReturnOk()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var cropTypes = new List<CropType>
+            {
+                new CropType { Id = 1, Name = "CropType1", Weather = "Weather1" },
+                new CropType { Id = 2, Name = "CropType2", Weather = "Weather2" }
+            };
+            var response = new Response<IEnumerable<CropType>> { WasSuccess = true, Result = cropTypes };
+            _mockCropTypesUnitOfWork.Setup(x => x.GetAsync(pagination)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetAsync(pagination);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(cropTypes, okResult?.Value);
+            _mockCropTypesUnitOfWork.Verify(x => x.GetAsync(pagination), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetAsync_Pagination_ShouldReturnBadRequest()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var response = new Response<IEnumerable<CropType>> { WasSuccess = false };
+            _mockCropTypesUnitOfWork.Setup(x => x.GetAsync(pagination)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetAsync(pagination);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+            _mockCropTypesUnitOfWork.Verify(x => x.GetAsync(pagination), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetPagesAsync_ShouldReturnOk()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var totalPages = 5;
+            var response = new Response<int> { WasSuccess = true, Result = totalPages };
+            _mockCropTypesUnitOfWork.Setup(x => x.GetTotalPagesAsync(pagination)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetPagesAsync(pagination);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(totalPages, okResult?.Value);
+            _mockCropTypesUnitOfWork.Verify(x => x.GetTotalPagesAsync(pagination), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetPagesAsync_ShouldReturnBadRequest()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var response = new Response<int> { WasSuccess = false };
+            _mockCropTypesUnitOfWork.Setup(x => x.GetTotalPagesAsync(pagination)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetPagesAsync(pagination);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+            _mockCropTypesUnitOfWork.Verify(x => x.GetTotalPagesAsync(pagination), Times.Once());
+        }
+    }
+}
